554c1847e17e2110e1d1f68cf776c9c1f5edee10
Harish Kp <harish-kp@users.noreply.github.com>
2021-02-13 16:57:09 +1100

    Task 1 Python complete and Task 2 test

---
+++ src/task11.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import rospy, tf
+import geometry_msgs.msg, nav_msgs.msg
+from math import *
+from time import sleep
+def huskyOdomCallback(message,cargs):
+
+    # Callback arguments 
+    pub,msg,call = cargs
+    default_v = 0 # default linear velocity
+    default_w = 0 # default angluar velocity
+    move_w = 0.5
+    if call == 1 :
+        print (" w = 0 ")
+        msg.linear.x = default_v
+        msg.angular.z = default_w
+        pub.publish(msg)
+    msg.linear.x = default_v
+    msg.angular.z = move_w
+    pub.publish(msg)
+def callback(event):
+    print("Rotated 10 sec using python")
+    rospy.signal_shutdown("Rotated 10 secs")  
+    
+call = 0  
+rospy.init_node('navigation_husky',anonymous=True)
+if call == 0:
+    rospy.Timer(rospy.Duration(10), callback)
+    cmdmsg = geometry_msgs.msg.Twist()
+    cmdpub = rospy.Publisher('/cmd_vel',geometry_msgs.msg.Twist, queue_size=10)
+    rospy.Subscriber('odometry/filtered',nav_msgs.msg.Odometry,huskyOdomCallback,(cmdpub,cmdmsg,call))
+    rospy.spin()
+else :
+    cmdmsg = geometry_msgs.msg.Twist()
+    cmdpub = rospy.Publisher('/cmd_vel',geometry_msgs.msg.Twist, queue_size=10)
+    rospy.Subscriber('odometry/filtered',nav_msgs.msg.Odometry,huskyOdomCallback,(cmdpub,cmdmsg,call)) 
+    rospy.signal_shutdown()
