cd6f8d284f3818201af7a73cd479ab9ef3a517bc
Harish Kp <harish-kp@users.noreply.github.com>
2021-02-17 17:44:37 +1100

    Task 1 completion
    Corresponding update of C++ file, CMakeLists and package files.
    Updated in Readme

--- README.md
+++ README.md
@@ -1,25 +1,26 @@
 # Practical-test
-#Task 1
+Status : In Development 
+Completed : Task 1
+In progress : Task 2
+Last update date : 17 February 2021
+# Task 1
 
 Task 1 aims to rotate the Husky UGV in clockwise and anticlockwise directions for 10 seconds. The second portion of the task aims to daemonize the task to be executed in the background without any interference from the terminal.
   
 Completed portion: 
-I have written python code to rotate the Husky UGV for 10 seconds in anticlockwise direction. In order to execute the code create a package named "practice_test" and clone the repository's contents into the package. "task1.py" file in the repository corresponds to this task.
+I have written python code to rotate the Husky UGV for 10 seconds in anticlockwise direction. In order to execute the code create a package named "practice_test" and clone the repository's contents into the package. "task1.py" file in the repository corresponds to this task. With the updated CMakeList and package file, creating a package in catkin_ws and `catkin_make` the process will compile the c++ and make it into an executable named `task`
   
 To execute the program, 
 In 3 different terminals,  
     Execute `roscore`  
-    Eexecute `roslaunch husky_gazebo husky_empty_world.launch`  
-    Get to the directory of the 'task1.py', excute `python task1.py`  
+    Execute `roslaunch husky_gazebo husky_empty_world.launch`  
+    Execute `rosrun practice_test task1.py` from `~/catkin_ws`  to run the python file (anticlockwise rotation)
+    Execute `rosrun practice_test task` from `~/catkin_ws` to execute the c++ (clockwise rotation)
     The file has already been daemonized, it'll right away exit to default terminal.  
-To review that the process was executed in the background, kindly use `ps -axl`, where you can lookup that \verbose{roslaunch husky\_gazebo husky\_empty\_world.launch} being executed for about 12 seconds.  
-Linux's "man daemon" came in handy to understand what and necessity of services in linux.  
+To review that the process was executed in the background, kindly use `ps -axl`, where you can lookup that \verbose{roslaunch husky\_gazebo husky\_empty\_world.launch} being executed for about 10 seconds and 12 seconds repectively.  
+Linux's "man daemon" came in handy to understand what and necessity of services in linux.    
 
-In progress: 
-
-I am continuing to develop the code of the same using c++ and create a linux service for it.  
-
-#Task 2
+# Task 2
 
 Task 2 aims to read goal from a csv file and write the poses traced by the robot back to the same csv file.
   
--- src/task1.cpp
+++ src/task1.cpp
@@ -2,55 +2,67 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <syslog.h>
 
+static void daemon()
+{
+    pid_t pid;
+
+    /* Fork off the parent process */
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    /* On success: The child process becomes session leader */
+    if (setsid() < 0)
+        exit(EXIT_FAILURE); }
 int main(int argc, char** argv)
 {
+  //Daemon function call
+  daemon();
   //init the ROS node
-  ROS_INFO_STREAM("Hello World");
   ros::init(argc, argv, "robot_driver");
   ros::NodeHandle nh;
 
   //init publisher
   ros::Publisher cmd_vel_pub_;
-  cmd_vel_pub_ = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
+  cmd_vel_pub_ = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
     
-  //init direction that turtlebot should go
+  //init direction that husky should go
   geometry_msgs::Twist base_cmd;
-  geometry_msgs::Twist base_cmd_turn_left;
+  geometry_msgs::Twist base_cmd_turn_right; //Clockwise rotation msg definition
 
   base_cmd.linear.x = 0;
   base_cmd.linear.y = 0;
   base_cmd.angular.z = 0;
-  base_cmd_turn_left.linear.x = 0; 
-  base_cmd_turn_left.linear.y = 0;
-  base_cmd_turn_left.angular.z = 0;
+  base_cmd_turn_right.linear.x = 0; 
+  base_cmd_turn_right.linear.y = 0;
+  base_cmd_turn_right.angular.z = 0;
 
-
-  //and let's go forward by setting X to a positive value
-  base_cmd.linear.x = 0.25;
-  base_cmd.angular.z = 0.0;
-  ROS_INFO_STREAM("And Crashing ... ctrl + c to stop me :)");
-
-  //base_cmd_turn_left will be used to turn turtlebot 90 degrees
-  base_cmd_turn_left.linear.x = 0; //m/s
-  base_cmd_turn_left.angular.z = 1.57/2; //45 deg/s * 2 sec = 90 degrees 
+  //base_cmd_turn_right will be used to turn husky 90 degrees
+  base_cmd_turn_right.linear.x = 0; //m/s
+  base_cmd_turn_right.angular.z = -1.57/2; 
 
   ros::Rate rate(5); // 5Hz
-
-  while(nh.ok()) { //have we ctrl + C?  If no... keep going!
-    //"publish" sends the command to turtlebot to keep going
-
-    //go forward for 2 seconds
-    for(int n=10; n>0; n--) {
-      cmd_vel_pub_.publish(base_cmd);
+  while(nh.ok()) { 
+    //"publish" sends the command to husky to keep going
+    for(int n=50; n>0; n--) {
+      cmd_vel_pub_.publish(base_cmd_turn_right);
       rate.sleep();
     }
-
-    //turn 90 degrees (takes 2 seconds)
-    for(int n=10; n>0; n--) {
-      cmd_vel_pub_.publish(base_cmd_turn_left);
-      rate.sleep();
-    }
+    break;
   }
 
 
@@ -58,8 +70,6 @@ int main(int argc, char** argv)
   base_cmd.linear.y = 0;
   base_cmd.angular.z = 0;
   cmd_vel_pub_.publish(base_cmd);
-
-  ROS_INFO("Finished\n");
 
   return 0;
 }
